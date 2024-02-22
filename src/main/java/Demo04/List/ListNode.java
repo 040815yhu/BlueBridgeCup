822bbef5a3a0d6bb6af69e73c8e2c29c1092bae5
YwaiX <3405230389@qq.com>
2024-02-22 19:29:42 +0800

    update Demo04

---
+++ src/main/java/Demo04/List/ListNode.java
@@ -0,0 +1,16 @@
+package Demo04.List;
+
+/**
+ * @Author YwaiX
+ * @Date 2024/2/22 17:33
+ * @Description ListNode
+ * 节点
+ */
+public class ListNode {
+    private Object data;
+    private ListNode next;
+    
+    public ListNode(Object data) {
+        this.data = data;
+    }
+}
---
+++ src/main/java/Demo04/List/SingleLinkedList.java
@@ -0,0 +1,43 @@
+package Demo04.List;
+
+/**
+ * @Author YwaiX
+ * @Date 2024/2/22 19:27
+ * @Description SingleLinkedList
+ */
+public class SingleLinkedList implements MyList {
+    @Override
+    public void add(Object Element) {
+    
+    }
+    
+    @Override
+    public void delete(Object Element) {
+    
+    }
+    
+    @Override
+    public void delete(int index) {
+    
+    }
+    
+    @Override
+    public void update(int index, Object newElement) {
+    
+    }
+    
+    @Override
+    public boolean contains(Object target) {
+        return false;
+    }
+    
+    @Override
+    public Object at(int index) {
+        return null;
+    }
+    
+    @Override
+    public int indexOf(Object element) {
+        return 0;
+    }
+}
